a5717a412477a8e634f0e5ee8109b5581992b852
Anurag Goel <59905+anurag@users.noreply.github.com>
2020-06-23 20:42:59 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hello from Render!
           </p>
           <a
             className="App-link"
