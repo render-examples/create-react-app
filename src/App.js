c7af389901e89a109cf5b3c9bca2f01fd5bc399b
Manu Arora <manuarorawork@gmail.com>
2022-02-24 10:33:30 +0530

    🚀 Test: CI/CD

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello from Render!
+            Hello from Render! Testing CI/CD on commits.
           </p>
           <a
             className="App-link"
