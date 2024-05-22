070cba931d5151b17d10b117aeac0583d7c14613
Carmel Paradis <16carmel@gmail.com>
2024-05-22 22:16:13 +0300

    initial stuff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react'
+import {useQuery} from 'react-query';
+
 import logo from './logo.svg';
 import './App.css';
+import {sayHelloWorld} from "./api/helloWorld";
 
-class App extends Component {
-  render() {
+function App() {
+    const query = useQuery('hello_world', sayHelloWorld);
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello from Render!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {query.isLoading ? "Loading..." : query.isError ? "Error..." :
+                        query.data}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
-  }
 }
 
 export default App;
---
+++ src/api/common.js
@@ -0,0 +1,3 @@
+export function getApiUrl() {
+    return process.env.REACT_APP_API_URL;
+}
